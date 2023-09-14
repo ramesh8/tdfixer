0d58d03d8d4acc4d78ae3daa77a51f46437757d0
Raja Ramesh Haddubangi <h.rajaramesh@gmail.com>
2023-09-15 02:35:39 +0530

    ui changes

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
+import itertools
 from bson import ObjectId
 from flask import Flask, render_template, jsonify, url_for, request
 from pymongo import MongoClient
+import spacy
+import json
+from spacy.tokens import DocBin
 
 app = Flask(__name__)
 mongoclient = MongoClient("mongodb://localhost:27017")
 
 db = mongoclient["test"]
 ents = db["entities"]
+
+nlp = spacy.blank("en")
 
 
 @app.route("/")
@@ -39,7 +45,50 @@ def remove_ent():
     id = data["id"]
     res = ents.update_one({"_id":ObjectId(id)},{"$pull":{"entities":ent}})
     # print(res)
-    return data
+    return {"res":res.modified_count}
+
+def validatetd(ent):
+    dbin = DocBin()
+    if "text" in ent:
+        text = ent["text"]
+    else:        
+        return False
+    if "entities" in ent:    
+        entlist = ent["entities"]
+    else:
+        return False
+
+    if len(entlist)==0: #means no entities marked
+        return False
+
+    entlist.sort()
+    unique_entities = [ent for ent,_ in itertools.groupby(entlist) ]
+    doc = nlp.make_doc(text)
+    valid_ents = []
+    try:
+        for start, end, label in unique_entities:
+            span = doc.char_span(start,end,label=label,alignment_mode="contract")            
+            if span is None:# or span.text.startswith(" ") or span.text.endswith(" ") :
+                return False
+            else:                
+                valid_ents.append(span)
+        doc.ents = valid_ents
+        dbin.add(doc)
+        return True
+    except Exception as ex:
+        return False
+    
+
+@app.route("/td/validate", methods=["GET","POST"])
+def validate():
+    data = request.get_json()    
+    id = data["id"]
+    res = ents.find_one({"_id":ObjectId(id)},{"_id":0})
+    # print(res)
+    isvalid = validatetd(res)
+
+    return {"valid":isvalid}
+
 
 @app.route('/favicon.ico')
 def favicon():
--- static/js/script.js
+++ static/js/script.js
@@ -4,18 +4,27 @@ var config = {
   fixedcount: 0,
   currentTD: null,
 };
+var errors = [
+  ["list-group-item-success", "✔"],
+  ["list-group-item-warning", "❗"],
+  ["list-group-item-danger", "❌"],
+];
 
-function displayTD(index) {
+function displayTD() {
   var textele = document.querySelector("#tdtext");
   var entsele = document.querySelector("#tdents");
-  var td = config["currentTD"];
+  if (config["currentTD"] == null) return;
+  var td = { ...config["currentTD"] };
   var _text = td["text"];
   textele.innerHTML = _text;
   entsele.innerHTML = "";
-  var prevent = null;
-  var mark = true;
+  document.querySelector("#eindex").innerHTML = config["currentIndex"];
   var max = _text.length;
-  for (ent of td["entities"]) {
+  var ents = td["entities"].slice();
+  var invalids = [],
+    counts = [0, 0, 0];
+  for (i = 0; i < ents.length; i++) {
+    ent = ents[i].slice();
     [start, end, label] = ent;
     start = parseInt(start);
     end = parseInt(end);
@@ -27,12 +36,14 @@ function displayTD(index) {
       start >= max ||
       end >= max
     ) {
-      ent.push("invalid");
+      // ent.push("invalid");
+      invalids.push(i);
       console.log("out of bounds error");
       continue;
     }
 
-    for (tent of td["entities"]) {
+    for (j = 0; j < ents.length; j++) {
+      tent = ents[j].slice();
       [tstart, tend, tlabel] = tent;
       tstart = parseInt(tstart);
       tend = parseInt(tend);
@@ -41,31 +52,73 @@ function displayTD(index) {
         ((start >= tstart && end <= tend) || (tstart >= start && tend <= end))
       ) {
         if (!ent.includes("invalid")) {
-          ent.push("invalid");
+          // ent.push("invalid");
+          invalids.push(j);
           console.log("overlap error");
           continue;
         }
       }
     }
-    if (!ent.includes("invalid")) ent.push("valid");
+    // if (!ent.includes("invalid")) ent.push("valid");
+
+    // var _ent = ent.slice(); //clone
+    // _ent.pop();
+    var error = 0;
+
+    var label = _text.slice(ent[0], ent[1]);
+    if (label[0] == " " || label[label.length - 1] == " ") error = 1;
+    if (invalids.includes(i)) error = 2;
 
     entsele.innerHTML += `<li onmouseover="hilit(this)" data-ent='${JSON.stringify(
       ent
-    )}' class="list-group-item list-group-item-action ${
-      ent.includes("valid")
-        ? "list-group-item-success"
-        : "list-group-item-danger"
-    }">${JSON.stringify(
+    )}' class="list-group-item list-group-item-action ${errors[error][0]}">${
+      errors[error][1]
+    } ${JSON.stringify(
       ent
-    )} <span class="controls float-end"><a class="btn btn-sm btn-outline-secondary me-2" onclick="editent(this)">✏</a>&nbsp;<a class="btn btn-sm btn-outline-secondary" onclick="removeent(this)">❌</a></li>`;
+    )} <span class="controls float-end"><a class="btn btn-sm btn-outline-secondary me-2" onclick="editent(this)">✏</a>&nbsp;<a class="btn btn-sm btn-outline-secondary" onclick="removeent(this)">✖</a></li>`;
+    counts[error] += 1;
   }
+  // console.log(config["currentTD"], "🍔");
+  sum = counts.reduce((a, b) => a + b, 0);
+  document.querySelector("#vents").innerHTML = counts[0];
+  document
+    .querySelector("#vents")
+    .style.setProperty(
+      "padding-top",
+      `${Math.round(50 * (counts[0] / sum))}px`
+    );
+  document.querySelector("#sents").innerHTML = counts[1];
+  document
+    .querySelector("#sents")
+    .style.setProperty(
+      "padding-top",
+      `${Math.round(50 * (counts[1] / sum))}px`
+    );
+  document.querySelector("#dents").innerHTML = counts[2];
+  document
+    .querySelector("#dents")
+    .style.setProperty(
+      "padding-top",
+      `${Math.round(50 * (counts[2] / sum))}px`
+    );
 }
 
 function loadnewtd(change) {
   if (config["currentIndex"] == 0 && change == -1) return;
   config["currentIndex"] += change;
+  updateLS();
   loadTD(config["currentIndex"]);
 }
+
+document.addEventListener("keydown", (ev) => {
+  const dir = ev.keyCode;
+  if (![37, 39, 13].includes(dir)) return;
+  ({
+    37: () => loadnewtd(-1),
+    39: () => loadnewtd(1),
+    13: () => validatetd(),
+  })[dir]();
+});
 
 function loadTD(index) {
   fetch(`/td/${index}`)
@@ -73,7 +126,7 @@ function loadTD(index) {
     .then((res) => {
       config["currentTD"] = res;
       updateLS();
-      displayTD(index);
+      displayTD();
     });
 }
 
@@ -83,13 +136,32 @@ function updateLS() {
 
 function validatetd() {
   //todo: run python script to check valid docbin
-  alert("not implemented yet");
+  var td = { ...config["currentTD"] };
+  var data = { id: td["_id"] };
+  fetch(`/td/validate`, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      isvalid = res["valid"];
+      var msg = isvalid ? "✔ Valid" : "❌ Invalid";
+      document.querySelector("#alertmsg").innerHTML = msg;
+
+      var myToastEl = document.querySelector("#liveToast");
+      var myToast = new bootstrap.Toast(myToastEl);
+      myToast.show();
+    });
 }
 window.onload = () => {
   if (localStorage.getItem("tdfixer") !== null) {
     config = JSON.parse(localStorage.getItem("tdfixer"));
   } else {
-    config["startIndex"] = 0;
+    config["currentIndex"] = 0;
     fetch("/tdbrokencount/")
       .then((res) => res.json())
       .then((res) => {
@@ -104,7 +176,7 @@ window.onload = () => {
       });
   }
 
-  loadTD(config["startIndex"]);
+  loadTD(config["currentIndex"]);
 };
 
 // document.querySelector(".list-group-item").addEventListener("click", (e) => {
@@ -115,12 +187,12 @@ function hilit(ele) {
   //   console.log(ele.dataset.ent);
   var _text = config["currentTD"]["text"];
   var ent = JSON.parse(ele.dataset.ent);
-  [s, e, l, v] = ent;
+  [s, e, l] = ent;
+  v = ele.className.split("-").slice(-1)[0];
+  console.log(v);
   var textele = document.querySelector("#tdtext");
   var s1 = _text.substring(0, s - 1);
-  var s2 = `<span class="${
-    v == "valid" ? "greenhilit" : "redhilit"
-  }">${_text.substring(s, e)}</span>`;
+  var s2 = `<span class="${v}-hilit">${_text.substring(s, e)}</span>`;
   var s3 = _text.substring(e + 1);
 
   _text = s1 + s2 + s3;
@@ -130,10 +202,16 @@ function hilit(ele) {
 
 function removeent(ele) {
   var parent = ele.parentNode.parentNode;
-  console.log(ele, parent);
-  var td = config["currentTD"];
+  // console.log(ele, parent);
+  var td = { ...config["currentTD"] };
   var ent = JSON.parse(parent.dataset.ent);
-  ent.pop();
+  console.log(ent);
+  newents = td.entities.filter((e) => JSON.stringify(e) != JSON.stringify(ent));
+  newents = newents.map((e) => e.slice(0, 3));
+  console.log(newents);
+  config["currentTD"]["entities"] = newents;
+  updateLS();
+  displayTD();
   var url = "/removeent/";
   var data = { id: td["_id"], ent: ent };
   //todo: post this data to url
